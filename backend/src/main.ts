e0391dc8bd58bddc0dd2d151b45f6531cc95c01f
StevenSsj1 <stevndevll22@gmail.com>
2025-04-22 12:18:10 -0500

    feat: refactor user module and integrate Prisma service with Swagger setup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+  }));
+
+  const config = new DocumentBuilder()
+  .setTitle('Prueba tecnica')
+  .setDescription('TODO TASK')
+  .setVersion('1.0')
+  .addTag('TASK')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors()
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((error) => {
--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,16 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
-    @IsEmail()
-    @IsNotEmpty()
-    @IsString()
-    email: string;
+  @IsEmail()
+  @IsNotEmpty()
+  @IsString()
+  email: string;
 
-    @IsNotEmpty()
-    @IsString()
-    password: string;
+  @IsNotEmpty()
+  @IsString()
+  password: string;
 
-    @IsNotEmpty()
-    @IsString()
-    organizationId: string;
+  @IsNotEmpty()
+  @IsString()
+  organizationId: string;
 }
